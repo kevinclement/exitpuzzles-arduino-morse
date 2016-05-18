570120ca13e6e81522b4e0b756440759bbc58023
Kevin Clement <kevin.clement@gmail.com>
2016-05-17 23:59:59 -0700

    cleanup 4, almost done with class

--- morseLip.cpp
+++ morseLip.cpp
@@ -2,32 +2,22 @@
 #include <Bounce2.h>
 
 // global defines
-#define DOT_TIME 75
-#define DASH_TIME 300
-
-#define DASH_TIME2 120
-
+#define DASH_TIME 120
+#define WAIT_THRESHOLD 1000
 
 // tmp
+unsigned long markTime = 0;
+unsigned long waitTime = 0;
+bool pressed = false;
 
 // Debounce stuff
 Bounce debouncer = Bounce(); 
-bool lastKeyerState = false;
-unsigned long lastDebounceTime = 0;  // the last time the input pin was toggled
-unsigned long debounceDelay = 20;
-bool morseSignalState = false;
 
-unsigned long markTime = 0;    // timers for mark and space in morse signal
-unsigned long spaceTime = 0;   // E=MC^2 ;p
-boolean gotLastSig = true;     // Flag that the last received morse signal is decoded as dot or dash
-
-// translation stuff
+// decode stuff
 char morseTable[] = "5H4S?V3I?F?U??2E?L?R???A?P?W?J1 6B?D?X?N?C?K?Y?T7Z?G?Q?M8??O9?0";
 const int morseTreetop = 31;   // character position of the binary morse tree top.
 int morseTableJumper = (morseTreetop + 1) / 2;
 int morseTablePointer = morseTreetop;
-
-unsigned long waitTime = 0;
 
 MorseLib::MorseLib(uint8_t pinIn, uint8_t speakerPin, bool echo)
 {
@@ -44,19 +34,11 @@ void MorseLib::setup()
   // setup speaker
   pinMode(_speakerPin, OUTPUT);
 
-  // variables used to track dot/dash
-  markTime = millis();
-  spaceTime = millis();
-
   // debounce setup
   debouncer.attach(_pinIn);
   debouncer.interval(20);
 }
 
-
-bool pressed = false;
-bool decodedIt = false;
-unsigned long markTime2 = 0;
 char MorseLib::getChar()
 {
   // empty char
@@ -66,19 +48,19 @@ char MorseLib::getChar()
   debouncer.update();
 
   // Get the updated value
-  morseSignalState = debouncer.read() == LOW; 
+  bool btnPressed = debouncer.read() == LOW; 
 
   // Output to speaker if enabled
   if (_echo) {
-    digitalWrite(_speakerPin, morseSignalState);
+    digitalWrite(_speakerPin, btnPressed);
   }
 
   // being pressed, just track it so we can decode it when not pressed
-  if (morseSignalState) {
+  if (btnPressed) {
 
     // first time changed state, mark time this happened
     if (!pressed) {
-      markTime2 = millis();
+      markTime = millis();
     }
     
     pressed = true;    
@@ -89,13 +71,11 @@ char MorseLib::getChar()
     if (pressed) {
       
       // determine if it was a dot or a dash
-      if (millis() - markTime2 > DASH_TIME2) {
-        Serial.println("DASH");
+      if (millis() - markTime > DASH_TIME) {
         morseTablePointer += morseTableJumper;
       } 
       else {
         morseTablePointer -= morseTableJumper;
-        Serial.println("DOT");
       }
       morseTableJumper /= 2;
 
@@ -104,9 +84,7 @@ char MorseLib::getChar()
     } else {
       
       // otherwise we're waiting, if this is first time, write down its time
-      if (waitTime > 0 && millis() - waitTime > 1000) {
-        Serial.println("DECODE IT");
-
+      if (waitTime > 0 && millis() - waitTime > WAIT_THRESHOLD) {
         morseChar = morseTable[morseTablePointer];
         morseTableJumper = (morseTreetop + 1) / 2;
         morseTablePointer = morseTreetop;
@@ -117,73 +95,6 @@ char MorseLib::getChar()
 
     pressed = false;
   }
-
-  
-  // ## DEBOUNCE ##################################################################################
-//  bool morseKeyer = !digitalRead(_pinIn); // inverted for active-low input
-//
-//  // If the switch changed, due to noise or pressing:
-//  if (morseKeyer != lastKeyerState)
-//  {
-//    lastDebounceTime = millis(); // reset timer
-//  }
-//
-//  if ((millis() - lastDebounceTime) > debounceDelay)
-//  {
-//    // whatever the reading is at, it's been there for longer
-//    // than the debounce delay, so take it as the actual current state:
-//    morseSignalState = morseKeyer;
-//    
-//    // differentiante mark and space times
-//    if (morseSignalState) markTime = lastDebounceTime; else spaceTime = lastDebounceTime;
-//  }
-
-  // ##############################################################################################
-
- 
-  // When not pressed, possibly need to decode what was pressed
-//  if (!morseSignalState)
-//  {
-//    if (!gotLastSig)
-//    {
-//      if (morseTableJumper > 0)
-//      {
-//        // if pause for more than half a dot, get what kind of signal pulse (dot/dash) received last
-//        if (millis() - spaceTime > DOT_TIME / 2)
-//        {
-//          // if signal for more than 1/4 dotTime, take it as a valid morse pulse
-//          if (spaceTime - markTime > DOT_TIME / 4)
-//          {
-//            // if signal for less than half a dash, take it as a dot, else if not, take it as a dash
-//            // (dashes can be really really long...)
-//            if (spaceTime - markTime < DASH_TIME / 2) morseTablePointer -= morseTableJumper;
-//            else morseTablePointer += morseTableJumper;
-//            morseTableJumper /= 2;
-//            gotLastSig = true;
-//          }
-//        }
-//      } else { // error if too many pulses in one morse character
-//        Serial.println("<ERROR: unrecognized signal!>");
-//        gotLastSig = true;
-//        morseTableJumper = (morseTreetop + 1) / 2;
-//        morseTablePointer = morseTreetop;
-//      }
-//    }
-//    
-//    // Once we've gone longer than 1 second without a press and we've started to get pressed, then decode
-//    if ((millis() - spaceTime >= 1000) && (morseTableJumper < 16))
-//    {
-//      morseChar = morseTable[morseTablePointer];
-//      morseTableJumper = (morseTreetop + 1) / 2;
-//      morseTablePointer = morseTreetop;
-//    }
-//  } else {
-//    // while there is a signal, reset some flags
-//    gotLastSig = false;
-//  }
-
-  // save last state of the morse signal for debouncing
-//  lastKeyerState = morseKeyer;
 
   return morseChar;
 }
